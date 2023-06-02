4d657c27b0f1f17bac5afee49a4d3e0814ec5a7d
Rice <fsyzxz@163.com>
2023-06-02 10:15:04 +0800

    修复支付宝展示键盘问题

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,5 +1,8 @@
 <template>
-	<view class="">
+	<view class="" :style="{backgroundColor: '#f7f7f7', fontSize}">
+		<div style="padding: 0.625rem 0;">
+			<div style="background-color: #ccc; padding: 0.625rem 0;">hahahaah</div>
+		</div>
 		<view class="example">
 			<navigator url="../tabs/v-tabs-1/v-tabs-1">v-tabs 1.x</navigator>
 			<navigator url="../tabs/tabs">v-tabs 基本示例</navigator>
@@ -11,6 +14,32 @@
 		</view>
 	</view>
 </template>
+
+<script>
+	export default {
+		data() {
+			return {
+				calcFontSize: 16
+			}
+		},
+		computed: {
+			fontSize() {
+				console.log(`${this.calcFontSize * 0.625}`);
+				return `${this.calcFontSize}px`
+			}
+		},
+		mounted() {
+			uni.getSystemInfo().then(result => {
+				const [err, res] = result
+				this.calcFontSize = res.screenWidth / 23.4375
+			})
+
+			uni.setNavigationBarTitle({
+				title: '哈哈哈哈'
+			})
+		}
+	}
+</script>
 
 <style lang="scss" scoped>
 	.example {
--- pages/keyboard/keyboard.vue
+++ pages/keyboard/keyboard.vue
@@ -1,29 +1,30 @@
 <template>
-  <view>
-    <div>
-      <span @click="show = true">点我调用键盘：{{ inputValue }}</span>
-    </div>
-    <v-keyboard type="tel" :show.sync="show" :value.sync="inputValue" @confirm="onConfirm" :max-length="11"></v-keyboard>
-  </view>
+	<view>
+		<div @click="toggleKeyboard()">
+			<span>点我调用键盘：{{ inputValue }}</span>
+		</div>
+		<v-keyboard type="tel" :show.sync="show" :value.sync="inputValue" @confirm="onConfirm"
+			:max-length="11"></v-keyboard>
+	</view>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      inputValue: '',
-      show: false
-    }
-  },
-  methods: {
-    onConfirm(value) {
-      alert(`当前输入完成，输入的内容是：${value}`)
-    },
-    toggleKeyboard() {
-      this.show = true
-    }
-  }
-}
+	export default {
+		data() {
+			return {
+				inputValue: '',
+				show: false
+			}
+		},
+		methods: {
+			onConfirm(value) {
+				alert(`当前输入完成，输入的内容是：${value}`)
+			},
+			toggleKeyboard() {
+				this.show = !this.show
+			}
+		}
+	}
 </script>
 
 <style lang=" scss" scoped></style>
